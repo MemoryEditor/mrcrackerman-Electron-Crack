c535efee9e9f943014598fcf4ba1229ff3ff9565
MemoryEditor <120334229+MemoryEditor@users.noreply.github.com>
2023-12-14 13:45:50 -0300

    Add files via upload

---
+++ mrcrackerman's Electron Crack/mrcrackerman's Electron Crack/mrcrackerman's Electron Crack.cpp
@@ -0,0 +1,27 @@
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Beginning Electron Crack v0.06.4" << std::endl;
+    Sleep(500);
+    std::cout << "Grabbing HWID" << std::endl;
+    Sleep(500);
+    std::cout << "Checking Time & Date" << std::endl;
+    Sleep(100);
+    std::cout << "Time Syncing is not required!" << std::endl;
+    std::cout << "Creating Custom Build for your HWID." << std::endl;
+    Sleep(100);
+    std::cout << "Creation Success! No errors! Starting compiler." << std::endl; // What ong, why starting compiler why would you need to recompile the "source code"? LOL
+    Sleep(100);
+    std::cout << "Successfully Compiled!" << std::endl; // 💀
+    Sleep(100);
+    std::cout << "Added Particle.exe and ElectronUI.exe to Temp." << std::endl; // ???
+    Sleep(100);
+    std::cout << "Running Particle.exe" << std::endl;
+    Sleep(100);
+    std::cout << "Injected kodiakcrack.dll to PID 2501." << std::endl; // What bro LOL
+    Sleep(100);
+    std::cout << "Bypassed all checks and cracked Electron successfully! Enjoy! (Upon Closing the window Electron will disappear from your Temp folder.)" << std::endl;
+    Sleep(200);
+}
